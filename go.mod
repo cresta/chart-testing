9f1133c640d6db81dd43d0a1de8e17453eb2c872
Jack Lindamood <jack@cresta.ai>
2021-11-29 17:44:29 +0000

    use mage for build

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cresta/chart-testing
+
+go 1.14
+
+require github.com/cresta/magehelper v0.0.30
---
+++ mage.go
@@ -0,0 +1,20 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	_ "github.com/cresta/magehelper/cicd/githubactions"
+	"github.com/cresta/magehelper/docker/registry"
+	"github.com/cresta/magehelper/env"
+	// mage:import docker
+	_ "github.com/cresta/magehelper/docker"
+	// mage:import ghcr
+	"github.com/cresta/magehelper/docker/registry/ghcr"
+)
+
+func init() {
+	// Install ECR as my registry
+	registry.Instance = ghcr.Instance
+	env.Default("DOCKER_MUTABLE_TAGS", "true")
+}
